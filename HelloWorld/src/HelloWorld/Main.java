1d6d1f14ab777c10b648ccff5728423133e963e5
Skytaker <gaki737@gmail.com>
2017-08-25 20:05:59 +0200

    Added first project

---
+++ HelloWorld/src/HelloWorld/Main.java
@@ -0,0 +1,34 @@
+//	I
+//	DO
+//	NOT
+//	NEED
+//	THE
+//	LICENSE
+//	HEADER
+
+package HelloWorld;
+
+
+//	AUTHOR
+//	IS
+//	NOT
+//	NEEDED
+//	AS
+//	WELL
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World!");
+	}
+	
+}
+
+
+//	btw, ich nutze aus erfahrung 
+//(mehrere repos wurden schon wegen der ach so tollen implementierung 
+//eines git clients in etwaigen programmen (eclipse, vs, etc.) 
+//zerschossen oder fehlerhaft gemerged)
+//das programm GitKraken als git client
+//hoffe dass dies nicht negativ ist
